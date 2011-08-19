40067a11aeb6334203dfe09d01fe501834a479a7
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-08-19 16:49:48 +0200

    Rewrote the lang/config files to stemmer drivers, put the English class into a vendor lib, added a config file and left only the ignore words in the lang files.

---
+++ classes/sitesearch/stemmer/english.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SiteSearch;
+
+class SiteSearch_Stemmer_English implements SiteSearch_Stemmer_Driver {
+	
+	public static function _init()
+	{
+		require_once PKGPATH.'sitesearch'.DS.'vendor'.DS.'porterstemmer.php';
+	}
+
+	public static function stem($word)
+	{
+		return PorterStemmer::stem($word, true);
+	}
+}
+
+// end of file english.php
---
+++ lang/dutch/sitesearch.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	// Verbs in noise word list: kunnen, mogen, hebben, willen, worden, zullen, zijn
+	'noise_words' => array(
+		'aan', 'alle', 'ben', 'bij', 'dan', 'dat', 'de', 'deze', 'die', 'dit', 'door', 'dus', 'een', 'elk', 'elke', 'en',
+		'ga', 'gaan', 'gaat', 'geen', 'ging', 'gingen', 'had', 'hadden', 'heb', 'hebben', 'hebt', 'het', 'hij', 'in', 'is', 
+		'je', 'kan', 'kon', 'konden', 'kunnen', 'kunt', 'maar', 'mag', 'maken', 'meer', 'met', 'moet', 'mocht', 'mochten', 
+		'moeten', 'mogen', 'naar', 'niet', 'nog', 'of', 'ook', 'op', 'over', 'te', 'tot', 'u', 'van', 'veel', 'voor', 
+		'vooral', 'waren', 'was', 'wat', 'welke', 'werd', 'werden', 'wil', 'wilde', 'willen', 'wilden', 'word', 'worden', 
+		'wordt', 'zal', 'zijn', 'zoals', 'zou', 'zouden', 'zullen',
+	),
+);
+
+// end of file sitesearch.php
